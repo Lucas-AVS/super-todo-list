402249e18a28ff1ab385f42d4dac341b500fc65a
Ponk8 <lucas.alencar31@gmail.com>
2024-03-16 20:04:18 -0300

    initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./style.css";
+
+const content = document.querySelector(".content");
+
+const test = document.createElement("h1");
+test.textContent = "This is a test";
+
+content.appendChild(test);
