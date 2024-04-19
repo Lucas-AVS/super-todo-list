cd78ac82b615185c582e9c163a80c557e927d742
Lucas-AVS <lucas.alencar31@gmail.com>
2024-04-19 15:29:46 -0300

    feat: Implemented function to create bar elements and custom projects are now stored in localStorage

--- src/components/sideBar.js
+++ src/components/sideBar.js
@@ -9,41 +9,26 @@ export default function sideBar() {
   const mainDiv = document.createElement('div')
   mainDiv.classList.add('side-bar')
 
-  const importantDiv = document.createElement('div')
-  importantDiv.classList.add('important-bar')
-  const importantIcon = document.createElement('i')
-  importantIcon.classList.add('fas', 'fa-star')
-  importantDiv.appendChild(importantIcon)
-  const importantText = document.createTextNode('Important')
-  importantDiv.appendChild(importantText)
+  function createElements(iconClasses, text) {
+    const div = document.createElement('div')
+    div.classList.add(`${text}Bar`, 'projectBar')
+    const i = document.createElement('i')
+    i.classList.add(...iconClasses)
+    div.appendChild(i)
+    const t = document.createTextNode(text)
+    div.appendChild(t)
+    mainDiv.appendChild(div)
+  }
 
-  const todayDiv = document.createElement('div')
-  todayDiv.classList.add('today-bar')
-  const todayIcon = document.createElement('i')
-  todayIcon.classList.add('far', 'fa-calendar-alt')
-  todayDiv.appendChild(todayIcon)
-  const todayText = document.createTextNode('Today')
-  todayDiv.appendChild(todayText)
-
-  const weekDiv = document.createElement('div')
-  weekDiv.classList.add('week-bar')
-  const weekIcon = document.createElement('i')
-  weekIcon.classList.add('far', 'fa-calendar-check')
-  weekDiv.appendChild(weekIcon)
-  const weekText = document.createTextNode('This Week')
-  weekDiv.appendChild(weekText)
-
-  const completedDiv = document.createElement('div')
-  completedDiv.classList.add('completed-bar')
-  const completedIcon = document.createElement('i')
-  completedIcon.classList.add('fas', 'fa-check-circle')
-  completedDiv.appendChild(completedIcon)
-  const completeText = document.createTextNode('Completed')
-  completedDiv.appendChild(completeText)
+  createElements(['fas', 'fa-star'], 'important')
+  createElements(['far', 'fa-calendar-alt'], 'today')
+  createElements(['far', 'fa-calendar-check'], 'thisWeek')
+  createElements(['fas', 'fa-tasks'], 'allTodos')
+  createElements(['fas', 'fa-check-circle'], 'completed')
 
   // New Project
   const newProjectDiv = document.createElement('div')
-  newProjectDiv.classList.add('new-project-bar')
+  newProjectDiv.classList.add('newProjectBar')
   const newProjectIcon = document.createElement('i')
   newProjectIcon.classList.add('fas', 'fa-folder-plus')
   newProjectDiv.appendChild(newProjectIcon)
@@ -55,27 +40,24 @@ export default function sideBar() {
   createButton.classList.add('create-button')
   createButton.textContent = 'Create'
   newProjectDiv.appendChild(createButton)
+  mainDiv.appendChild(newProjectDiv)
+
+  // Load custom projects from localsotrage
+  let customProjects = JSON.parse(localStorage.getItem('customProjects')) || []
+  customProjects.forEach((project) => {
+    createElements(['fas', 'fa-folder'], project)
+  })
 
   function createProject(inputValue) {
-    const newProject = document.createElement('div')
-    newProject.classList.add(`${inputValue}-bar`, 'project-bar')
-    const newProjectIcon = document.createElement('i')
-    newProjectIcon.classList.add('fas', 'fa-folder')
-    newProject.appendChild(newProjectIcon)
-    const newProjectText = document.createTextNode(inputValue)
-    newProject.appendChild(newProjectText)
-    mainDiv.appendChild(newProject)
+    customProjects.push(inputValue)
+    localStorage.setItem('customProjects', JSON.stringify(customProjects))
+    createElements(['fas', 'fa-folder'], inputValue)
   }
   createButton.addEventListener('click', () =>
     createProject(newProjectInput.value)
   )
 
   // Append all the divs to the main div
-  mainDiv.appendChild(importantDiv)
-  mainDiv.appendChild(todayDiv)
-  mainDiv.appendChild(weekDiv)
-  mainDiv.appendChild(completedDiv)
-  mainDiv.appendChild(newProjectDiv)
 
   content.appendChild(mainDiv)
 }
